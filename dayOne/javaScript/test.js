6ef805460865edd07c1686c13d3d083f8ad3da54
Wheeny <winiefrednnaji@gmail.com>
2026-08-17 14:41:13 +0100

    Push

---
+++ dayOne/javaScript/test.js
@@ -0,0 +1,6 @@
+/*
+javac -cp "junit-platform-console-standalone-1.11.0.jar:out" -d out PositiveIntegersRange.java PositiveIntegersRangeTest.java
+
+java -cp "junit-platform-console-standalone-1.11.0.jar:out" org.junit.platform.console.ConsoleLauncher --scan-class-path
+
+*/
---
+++ dayOne/python/test.py
@@ -0,0 +1,18 @@
+#import unittest
+from unittest import TestCase
+from account_function import * #check_balance, deposit
+
+class AccountTest(TestCase):
+
+    def test_accountBalance_isZero_duringCreation(self):
+        
+        expected_balance = 0
+        actual_balance = check_balance(0.0)
+        self.assertEqual(actual_balance, expected_balance)
+        
+        
+        
+#        """
+#        run on terminal with: python3 -m unittest account_function_test.py
+#
+#        """        
